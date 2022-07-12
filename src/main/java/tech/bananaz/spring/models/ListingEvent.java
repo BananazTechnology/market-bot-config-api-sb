474d84b20a39d8776a96736853539d47f656a0a8
Aaron Renner <arennerwv@gmail.com>
2022-07-12 10:28:16 -0400

    2.2.0 reduced DB column sizing requirement

--- src/main/java/tech/bananaz/spring/models/ListingEvent.java
+++ src/main/java/tech/bananaz/spring/models/ListingEvent.java
@@ -22,34 +22,49 @@ public class ListingEvent {
 	
 	@Id
 	private long       id;
+	@Column(columnDefinition = "VARCHAR(75)")
 	private String     name;
 	private Instant    createdDate;
 	private Instant    startTime;
 	private Instant    endTime;
 	private String     tokenId;
+	@Column(columnDefinition = "VARCHAR(75)")
 	private String     collectionName;
 	private String     collectionImageUrl;
+	@Column(columnDefinition = "VARCHAR(50)")
 	private String     slug;
 	private String     imageUrl;
 	private String     permalink;
 	private int	       quantity;
+	@Column(columnDefinition = "VARCHAR(50)")
 	private String     sellerWalletAddy;
+	@Column(columnDefinition = "VARCHAR(50)")
 	private String     sellerName;
 	private String     sellerUrl;
+	@Column(columnDefinition = "VARCHAR(50)")
 	private String     displayNameOutput;
+	@Column(columnDefinition = "VARCHAR(6)")
 	private String     rarity;
+	@Column(columnDefinition = "VARCHAR(50)")
 	private String     rarityRedirect;
+	@Column(columnDefinition = "VARCHAR(25)")
 	private BigDecimal listingPriceInCrypto;
+	@Column(columnDefinition = "VARCHAR(25)")
 	private BigDecimal listingPriceInUsd;
+	@Column(columnDefinition = "VARCHAR(75)")
 	private String     listingAmoutOutput;
 	@Enumerated( EnumType.STRING )
+	@Column(columnDefinition = "VARCHAR(6)")
 	private Ticker     cryptoPaymentType;
 	@Enumerated( EnumType.STRING )
+	@Column(columnDefinition = "VARCHAR(50)")
 	private RarityEngine engine;
 	@Enumerated( EnumType.STRING )
+	@Column(columnDefinition = "VARCHAR(50)")
 	private MarketPlace  market;
 	// These last few items are for the consumer
 	private long         contractId;
+	@Column(columnDefinition = "VARCHAR(50)")
 	private String       consumedBy;
 	@Column(nullable = false, columnDefinition="TINYINT(1) UNSIGNED DEFAULT 0")
 	private boolean      consumed;
